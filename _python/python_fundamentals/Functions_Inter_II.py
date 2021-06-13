aaeaa83b1b6f4f2314216dea076f2c1cbd9496f7
Nathan Denni <aubakasan303@gmail.com>
2021-06-14 08:04:32 +0900

    Functions_inter_II updated Dic with list

--- _python/python_fundamentals/Functions_Inter_II.py
+++ _python/python_fundamentals/Functions_Inter_II.py
@@ -39,6 +39,12 @@ print("\r")
 
 # Iterate Through a List of Dictionaries
     # Create a function iterateDictionary(some_list) that, given a list of dictionaries, the function loops through each dictionary in the list and prints each key and the associated value. For example, given the following list:
+    # should output: (it's okay if each key-value pair ends up on 2 separate lines;
+    # bonus to get them to appear exactly as below!)
+    # first_name - Michael, last_name - Jordan
+    # first_name - John, last_name - Rosales
+    # first_name - Mark, last_name - Guillen
+    # first_name - KB, last_name - Tonel
 
 students = [
         {'first_name' : 'Michael', 'last_name' : 'Jordan'},
@@ -53,10 +59,33 @@ def iterateDictionary(x):
 iterateDictionary(students)
 print("\r")
 
+# Get Values From a List of Dictionaries
+    # Create a function iterateDictionary2(key_name, some_list) that, given a list of dictionaries and a key name, the function prints the value stored in that key for each dictionary. For example, iterateDictionary2('first_name', students) should output:
+    # Michael
+    # John
+    # Mark
+    # KB
 
-# should output: (it's okay if each key-value pair ends up on 2 separate lines;
-# bonus to get them to appear exactly as below!)
-    # first_name - Michael, last_name - Jordan
-    # first_name - John, last_name - Rosales
-    # first_name - Mark, last_name - Guillen
-    # first_name - KB, last_name - Tonel
+def iterateDictionary2(key_name, some_list):
+    for i in range(len(some_list)):
+            print(some_list[i][key_name])
+
+names = iterateDictionary2('last_name', students)
+print("\r")
+
+# Iterate Through a Dictionary with List Values
+    # Create a function printInfo(some_dict) that given a dictionary whose values are all lists, prints the name of each key along with the size of its list, and then prints the associated values within each key's list. For example:
+
+dojo = {
+    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+def printInfo(x):
+    for key, values in x.items():
+        print("\r")
+        print(str(len(values))+" "+key.upper())
+        print("\n".join(values))
+
+printInfo(dojo)
+
+print("\r")
