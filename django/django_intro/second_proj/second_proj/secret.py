d65e685cc2c01060843bbd60798a0b19924f2e8b
Nathan Denni <aubakasan303@gmail.com>
2021-06-23 18:12:44 +0900

    first commit of time display. update first proj

---
+++ django/django_intro/second_proj/second_proj/secret.py
@@ -0,0 +1,2 @@
+
+SECRET_KEY = 'b3r=p3a3@qb2e+0x4jj!o8rumd$omrvcytknxui@pgua4yque3'
--- django/django_intro/second_proj/time_display/views.py
+++ django/django_intro/second_proj/time_display/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, redirect, HttpResponse
 from time import localtime, strftime
+from django.utils import timezone
+
+now = timezone.now()
 
 def index(request):
     context = {
-        "time": strftime("%a %Y-%m-%d %H:%M %p", localtime())
+        "time": strftime("%a %Y-%m-%d %H:%M %p", localtime()),
     }
     return render(request, 'index.html', context)
 
