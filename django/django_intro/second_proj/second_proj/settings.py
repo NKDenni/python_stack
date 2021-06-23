94b7e18e3df3e999a5a889639e466f8fdb6bef26
Nathan Denni <aubakasan303@gmail.com>
2021-06-23 14:39:40 +0900

    Removed Secrets

--- django/django_intro/second_proj/second_proj/settings.py
+++ django/django_intro/second_proj/second_proj/settings.py
@@ -21,7 +21,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'b3r=p3a3@qb2e+0x4jj!o8rumd$omrvcytknxui@pgua4yque3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
