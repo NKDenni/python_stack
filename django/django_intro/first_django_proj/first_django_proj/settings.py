d0354272741a5fccdf86a973ba4d49fe011fe3cb
Nathan Denni <aubakasan303@gmail.com>
2021-06-23 14:44:54 +0900

    Removed secrets

--- django/django_intro/first_django_proj/first_django_proj/settings.py
+++ django/django_intro/first_django_proj/first_django_proj/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from secrets import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')b^fg-#+-*+868^+o+w_y7m=+0$lkq5&4wmnumzag7a$pdbgv7'
+#removed from file.
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
